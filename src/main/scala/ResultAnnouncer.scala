fe7bc450739efe9cd79bf36627b171526a7bdae4
Erdem Agaoglu <erdem.agaoglu@uzay.tubitak.gov.tr>
2011-07-01 12:09:42 +0300

    No scaml templates to render

--- src/main/scala/ResultAnnouncer.scala
+++ src/main/scala/ResultAnnouncer.scala
@@ -24,20 +24,4 @@ class ResultAnnouncer extends ScalatraFilter with ScalateSupport {
         "/WEB-INF/scalate/templates/main.jade", Map("formErr"->"ID Number and/or password is wrong"))
     }
   }
-
-  notFound {
-    // If no route matches, then try to render a Scaml template
-    val templateBase = requestPath match {
-      case s if s.endsWith("/") => s + "index"
-      case s => s
-    }
-    val templatePath = "/WEB-INF/scalate/templates/" + templateBase + ".scaml"
-    servletContext.getResource(templatePath) match {
-      case url: URL => 
-        contentType = "text/html"
-        templateEngine.layout(templatePath)
-      case _ => 
-        filterChain.doFilter(request, response)
-    } 
-  }
 }
