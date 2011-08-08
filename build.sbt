7605a8b23f7544fc24d5fd1ebe35f212409c0e4f
Erdem Agaoglu <erdem.agaoglu@gmail.com>
2011-08-08 15:55:38 +0300

    Cassandra working

--- build.sbt
+++ build.sbt
@@ -12,7 +12,8 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % "2.0.0-SNAPSHOT",
   "org.scalatra" %% "scalatra-scalate" % "2.0.0-SNAPSHOT",
   "org.mortbay.jetty" % "jetty" % "6.1.22" % "jetty",
-  "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
+  "javax.servlet" % "servlet-api" % "2.5" % "provided->default",
+  "me.prettyprint" % "hector-core" % "0.7.0-30" 
 )
 
 resolvers ++= Seq(
--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -1,16 +1,32 @@
-import scala.collection.mutable.Map
+import me.prettyprint.hector.api._
+import factory._
+import ddl._
+import me.prettyprint.cassandra.service._
+import template._
+import me.prettyprint.cassandra.serializers.StringSerializer
 
 import java.security.MessageDigest
+import java.util.Arrays
 
 object Result {
-  val store = Map[String, (String, String, String, String)]()
+  val cluster = HFactory.getOrCreateCluster("test-cluster", "localhost:9160")
+  val keySpace = "RA"
+  var _ksp: Keyspace = null
+  var _template: ColumnFamilyTemplate[String, String] = null
+  def ksp = {
+    if (_ksp == null) 
+      _ksp = HFactory.createKeyspace(keySpace, cluster)
+    _ksp
+  }
+  def template = {
+    if (_template == null) 
+      _template = new ThriftColumnFamilyTemplate[String, String](ksp, "f", StringSerializer.get(), StringSerializer.get())
+    _template
+  }
 
   def of(id:String, passwd:String) = {
-    val data = store.get(id)
-    data match {
-      case Some(_) => render(data.get, id)
-      case _ => None
-    }
+    val res = template.queryColumns(id);
+    render((id, res.getString("p"), res.getString("n"), res.getString("r")) ,passwd)
   }
   
   def render(data: (String,String,String,String), passwd: String) =
@@ -18,11 +34,25 @@ object Result {
   
   def preload() {
     println("Loading db into mem")
+    if (cluster.describeKeyspace(keySpace) == null)
+      createSchema();
+    
     for {
         l <- scala.io.Source.fromFile("db").getLines(); 
-        val p = l.split(",").toList}
-      store += p(0) -> (p(0), p(1), p(2), p(3))
+        val p = l.split(",").toList;
+        val u = template.createUpdater(p(0))} {
+      u.setString("p", p(1))
+      u.setString("n", p(2))
+      u.setString("r", p(3))
+      template.update(u)
+    }
     println("Loading - done")
+  }
+  
+  def createSchema() {
+    val cfDef = HFactory.createColumnFamilyDefinition(keySpace, "f", ComparatorType.BYTESTYPE);
+    val keyspace = HFactory.createKeyspaceDefinition(keySpace, ThriftKsDef.DEF_STRATEGY_CLASS, 1, Arrays.asList(cfDef));
+    cluster.addKeyspace(keyspace);
   }
 
   def md5(s:String) = {
